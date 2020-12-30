7ec2f18b13869e167c8a3c04695b57b43ebbb02d
aringojuice <76545550+aringojuice@users.noreply.github.com>
2020-12-30 12:39:57 +0700

    Many under the hood changes
    Section + anim + padding etc.

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,21 @@
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+});
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    $('#navbar').removeClass('scrolled-down').addClass('scrolled-up');
-  } else {
-    $('#navbar').removeClass('scrolled-up').addClass('scrolled-down');
-  }
-  prevScrollpos = currentScrollPos;
+    if (prevScrollpos > currentScrollPos) {
+        $('#navbar').removeClass('scrolled-down').addClass('scrolled-up');
+    } else {
+        $('#navbar').removeClass('scrolled-up').addClass('scrolled-down');
+    }
+    prevScrollpos = currentScrollPos;
+}  
+
+function focusSearch(){
+    var menu = $('.menu');
+    var searchbar = $('.search-bar');
+    menu.removeClass('visible').addClass('invisible');
+    searchbar.removeClass('invisible').addClass('visible');
 }
