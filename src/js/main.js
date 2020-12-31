91f120c8f2a20ea5ceb793372c63ec983051137e
ahm4d <dania9015@gmail.com>
2020-12-31 23:57:58 +0700

    menambah fungsi hide and show search

--- src/js/main.js
+++ src/js/main.js
@@ -14,8 +14,40 @@ window.onscroll = function() {
 }  
 
 function focusSearch(){
-    var menu = $('.menu');
-    var searchbar = $('.search-bar');
+    var menu = $('#navbarNav');
+    var searchbar = $('#buttonSea');
+    var fieTex = $('#textFi');
     menu.removeClass('visible').addClass('invisible');
     searchbar.removeClass('invisible').addClass('visible');
+    fieTex.removeClass('invisible').addClass('visible');
+    $(function(){
+        $('#buttonSea').click(function(e){
+            $('#navbarNav').addClass('visible');
+            $('#textFi').addClass('invisible');
+        });
+    });
+
+    /*if($('#').click(function())){
+        console.log('jhajaj');
+        menu.addClass('visible');
+    }else if($('#buttonSea').click()){
+        searchbar.addClass('visible');
+    }else if($('#textFi').click()){
+        fieTex.addClass('visible');
+    }*/
+}
+
+function fun(){
+    var hasil = document.getElementById('buttonSea');
+    var txtField = document.getElementById('isi');
+    var conNav = document.getElementById('def');
+    var nav = document.getElementById('nb');
+    hasil.onclick = function(){
+        txtField.setAttribute('class','visible');
+        conNav.setAttribute('class','invisible');
+    };
+    nav.onclick = function(){
+        txtField.setAttribute('class','invisible');
+        conNav.setAttribute('class','visible');
+    };
 }
